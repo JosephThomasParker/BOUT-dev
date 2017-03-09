1dffa73d4aebd5bfb25279f8ecf52a8044e3a6f9
Joseph Parker <joseph.parker@stfc.ac.uk>
2017-03-09 17:41:40 +0000

    Edit test-staggered to test the interp_to function.
    NB Added [mesh] to input file, otherwise StaggerGrid variable not
    loaded.

--- examples/test-staggered/test_staggered.cxx
+++ examples/test-staggered/test_staggered.cxx
@@ -6,12 +6,14 @@
 #include <boutmain.hxx>
 
 #include <derivs.hxx>
+#include <interpolation.hxx>
 
-Field3D n, v;
+Field3D n, v, n_stag;
 
 int physics_init(bool restart) {
   
   v.setLocation(CELL_YLOW); // Staggered relative to n
+  n_stag.setLocation(CELL_YLOW); // Staggered relative to n
   
   SOLVE_FOR2(n, v);
 
@@ -20,11 +22,13 @@ int physics_init(bool restart) {
 
 int physics_run(BoutReal time) {
   mesh->communicate(n, v);
-  
+  n_stag = interp_to(n,CELL_YLOW);
+
   //ddt(n) = -Div_par_flux(v, n, CELL_CENTRE);
   ddt(n) = -n*Grad_par(v, CELL_CENTRE) - Vpar_Grad_par(v, n, CELL_CENTRE);
   
-  ddt(v) = -Grad_par(n, CELL_YLOW);
+  //ddt(v) = -Grad_par(n, CELL_YLOW);
+  ddt(v) = -Grad_par(n_stag);
  
   // Have to manually apply the lower Y boundary region, using a width of 3
   for( RangeIterator rlow = mesh->iterateBndryLowerY(); !rlow.isDone(); rlow++)
